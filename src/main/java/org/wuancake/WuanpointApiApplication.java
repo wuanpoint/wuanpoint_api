b123d74937f86f7ec3a31eff4cdbec98cf7b3c32
Ericheel <283773833@qq.com>
2019-01-24 13:22:59 +0800

    基础组件

---
+++ src/main/java/org/wuancake/WuanpointApiApplication.java
@@ -0,0 +1,18 @@
+package org.wuancake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 午安点评SpringBoot生成的启动类
+ * @author SpringBoot
+ */
+@SpringBootApplication
+public class WuanpointApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuanpointApiApplication.class, args);
+	}
+
+}
+
