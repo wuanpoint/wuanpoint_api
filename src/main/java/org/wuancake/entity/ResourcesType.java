3fe442b8145b15515d0e5f421e2a8fb8e54b0f87
253506088 <2842513372@qq.com>
2019-03-05 18:50:52 +0800

    新增pojo

---
+++ src/main/java/org/wuancake/entity/ResourcesType.java
@@ -0,0 +1,12 @@
+package org.wuancake.entity;
+
+import lombok.Data;
+
+/**
+ * 资源类型
+ */
+@Data
+public class ResourcesType {
+    private Integer typeId;//分类id
+    private String typeName;//分类名称
+}
