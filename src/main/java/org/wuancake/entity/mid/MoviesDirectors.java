2611779d4471b9c410099c510289acdd0a2bce97
253506088 <2842513372@qq.com>
2019-03-05 18:07:01 +0800

    修改后的pojo

--- src/main/java/org/wuancake/entity/mid/MoviesDirectors.java
+++ src/main/java/org/wuancake/entity/mid/MoviesDirectors.java
@@ -1,8 +1,22 @@
 package org.wuancake.entity.mid;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by ericheel on 2019/2/18.
  * 影片——导演
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@XmlRootElement
 public class MoviesDirectors {
+    private int movieId;//影片id
+    private int directorId;//导演id
 }
--- src/main/java/org/wuancake/entity/mid/MoviesType.java
+++ src/main/java/org/wuancake/entity/mid/MoviesType.java
@@ -1,8 +1,22 @@
 package org.wuancake.entity.mid;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by Administrator on 2019/2/18.
  * 影片——类型
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+@ToString
 public class MoviesType {
+    private int moviesId;//影片id
+    private int genresId;//影片类型id
 }
