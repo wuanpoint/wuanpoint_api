61fed17573e861c1cdfc17c95c314ea94e0ed9b9
0XCAFEBABE <283773833@qq.com>
2019-04-15 21:28:07 +0800

    修改

--- src/main/java/org/wuancake/WuanpointApiApplication.java
+++ src/main/java/org/wuancake/WuanpointApiApplication.java
@@ -2,12 +2,15 @@ package org.wuancake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 /**
  * 午安点评SpringBoot生成的启动类
  * @author SpringBoot
  */
 @SpringBootApplication
+@PropertySource("file:/etc/wuanpoint_application.yml")
 public class WuanpointApiApplication {
 
 	public static void main(String[] args) {
